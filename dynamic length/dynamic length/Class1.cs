1950337dae46260cbd9ac198b616d3b2eec614aa
CoolYeebOnline <55756139+CoolYeebOnline@users.noreply.github.com>
2019-11-08 13:16:11 +0000

    dynamic length

---
+++ dynamic length/dynamic length/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dynamic_length
+{
+    class Class1
+    {
+        object[] objArray = new object[0];
+        
+        private void Push(object item)
+        {
+            object[] pushArray = new object[objArray.Length+1];
+
+            for (int i = 0; i < objArray.Length; i++)
+            {
+                pushArray[i] = objArray[i];
+            }
+            pushArray[pushArray.Length - 1] = item;
+
+            objArray = pushArray;
+
+            object Pop()
+            {
+                object[] popArray = new object[pushArray.Length - 1];
+           
+                for (int i = 0; i < popArray.Length; i++)
+                {
+                    popArray[i] = pushArray[i];
+           
+                }
+                object returnValue = pushArray[pushArray.Length - 1];
+                pushArray = popArray;
+                return returnValue;
+            }
+        }
+        
+    }
+    
+}
---
+++ dynamic length/dynamic length/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace dynamic_length
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+
+            objArray[0] = "Hello";
+            objArray[1] = 7;
+            objArray[2] = 2.0f;
+            objArray[3] = true;
+            objArray[4] = 'c';
+
+           
+            
+        }
+        
+    }
+}
